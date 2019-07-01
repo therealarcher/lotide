6d4217548475d71d492af88ac597806ad1026d68
Jonathan Archer <jonathan.d.archer@gmail.com>
2019-07-01 18:51:45 +0000

    added tests to file

--- test/headTest.js
+++ test/headTest.js
@@ -1,12 +1,13 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head')
+const assert = require('chai').assert;
+const head   = require('../head');
 
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-// assertEqual(5, "5");
-// assertEqual("Hello", "hello");
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5]), 5);
-assertEqual(head([]),);
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+});
