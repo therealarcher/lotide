7c2169de48550446c439f323cc4cd41728634b29
Jonathan Archer <jonathan.d.archer@gmail.com>
2019-07-01 16:35:38 +0000

    modified head and tail files to not have assertions baked into the file

--- head.js
+++ head.js
@@ -1,21 +1,27 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
+
+const assertEqual = require('./assertEqual');
 
 const head = function(array) {
   return array[0];
-}
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(5, "5");
-assertEqual("Hello", "hello");
+};
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5]), 5);
-assertEqual(head([]),);
+module.exports = head;
+
+
+// TEST CODE
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+// assertEqual(5, "5");
+// assertEqual("Hello", "hello");
+
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([5]), 5);
+// assertEqual(head([]),);
--- tail.js
+++ tail.js
@@ -11,23 +11,4 @@ const tail = function(array) {
   return newArray;
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(5, "5");
-assertEqual("Hello", "hello");
-tail([1,2,3,4,5]);
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-const empty = [];
-tail(empty);
-assertEqual(empty.length, 0);
-
-const single = [234];
-tail(single);
-assertEqual(single.length, 1);
-
+module.exports = tail;
